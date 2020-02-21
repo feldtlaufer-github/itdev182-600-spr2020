92d1295111d1f622c111facf5af6254133716bd8
Alex Muhich <feldtlaufer@gmail.com>
2020-02-20 20:53:34 -0600

    page 65 pre location data

--- a2/weather/App.js
+++ a2/weather/App.js
@@ -3,30 +3,54 @@ import { StyleSheet, Text, ImageBackground, View, KeyboardAvoidingView, Platform
 import SearchInput from "./components/SearchInput";
 import getImageForWeather from "./utils/getImageForWeather";
 
-export default function App() {
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="height">
-      <ImageBackground
-        source={getImageForWeather("Clear")}
-        style={styles.imageContainer}
-        imageStyle={styles.image}>
-        <View style={styles.detailsContainer}>
-          <Text style={[styles.largeText, styles.textStyle]}>
-            San Francisco
-          </Text>
-          <Text style={[styles.smallText, styles.textStyle]}>
-            Light Cloud
-          </Text>
-          <Text style={[styles.largeText, styles.textStyle]}>
-            24°
-          </Text>
-          <SearchInput placeholder="Search any city" />
-        </View>
-      </ImageBackground>
-    </KeyboardAvoidingView>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      location: "San Francisco",
+    };
+  }
+  handleUpdateLocation = city => {
+    this.setState({
+      location: city,
+    });
+  };
+
+  render(){
+    const{ location } = this.state;
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="height">
+  
+        <ImageBackground
+          source={getImageForWeather("Clear")}
+          style={styles.imageContainer}
+          imageStyle={styles.image}>
+  
+          <View style={styles.detailsContainer}>
+  
+            <Text style={[styles.largeText, styles.textStyle]}>
+              {location}
+            </Text>
+  
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+  
+            <Text style={[styles.largeText, styles.textStyle]}>
+              24°
+            </Text>
+  
+            <SearchInput
+              placeholder="Search any city"
+              onSubmit={this.handleUpdateLocation} />
+  
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -71,7 +95,7 @@ const styles = StyleSheet.create({
     height: 40,
     width: 300,
     marginTop: 20,
-    maginHorizontal: 20,
+    marginHorizontal: 20,
     paddingHorizontal: 10,
     alignSelf: "center",
   },
