a0138a1f9f5d1a8f7acf8bb2577cbdef58791852
Alex Muhich <feldtlaufer@gmail.com>
2020-03-04 16:26:28 -0600

    toggleabletimerform inverse data flow

--- a3/time-tracking/components/ToggleableTimerForm.js
+++ a3/time-tracking/components/ToggleableTimerForm.js
@@ -12,14 +12,30 @@ export default class ToggleableTimerForm extends React.Component{
         this.setState({ isOpen: true });
     };
 
+    handleFormClose = () => {
+        this.setState({ isOpen: false });
+    };
+
+    handleFormSubmit = timer => {
+        const { onFormSubmit } = this.props;
+
+        onFormSubmit(timer);
+        this.setState({ isOpen: false });
+    };
+
     render(){
         const { isOpen } = this.state;
         return (
             <View style= {[styles.container, !isOpen && styles.buttonPadding]}>
                 {isOpen ? (
-                    <TimerForm />
+                    <TimerForm
+                        onFormSubmit={this.handleFormSubmit}
+                        onformClose={this.handleFormClose}/>
                 ) : (
-                    <TimerButton title="+" color="black" onPress={this.handleFormOpen} />
+                    <TimerButton
+                        title="+"
+                        color="black"
+                        onPress={this.handleFormOpen} />
                 )}
             </View>
         );
