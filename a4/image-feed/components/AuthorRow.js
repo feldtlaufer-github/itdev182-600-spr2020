972cfa0f6e2d1fbe8907466aca3751821f6b7f86
Alex Muhich <feldtlaufer@gmail.com>
2020-03-05 19:15:24 -0600

    pre-touchable opacity

---
+++ a4/image-feed/components/AuthorRow.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Avatar from './Avatar';
+import getAvatarColor from '../utils/getAvatarColor';
+import getInitials from '../utils/getInitials';
+
+export default function AuthorRow({
+    fullname, linkText, onPressLinkText
+}){
+    return (
+        <View style={styles.container}>
+            <Avatar
+                siz={35}
+                initials={getInitials(fullname)}
+                backgroundColor={getAvatarColor(fullname)} />
+                <Text style={styles.text} numberOfLines={1}>
+                    {fullname}
+                </Text>
+        </View>
+    );
+}
+
+AuthorRow.propTypes = {
+    fullname: PropTypes.string.isRequired,
+    linkText: PropTypes.string.isRequired,
+    onPressLinkText: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+    },
+    text: {
+        flex: 1,
+        marginHorizontal: 6,
+    },
+});
