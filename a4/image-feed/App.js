7c73e40b0501a83904b57e70211ce7cb54b43eda
Alex Muhich <feldtlaufer@gmail.com>
2020-03-05 18:24:28 -0600

    test avatar made pg188 is new starting point

--- a4/image-feed/App.js
+++ a4/image-feed/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/Avatar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  
+  render(){
+    return (
+      <View style={styles.container}>
+        <Avatar initials='FL' size={35} backgroundColor={'teal'} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
