f97f497d7fc94f7b82ad52cc0a4a5b51680b7166
Alex Muhich <feldtlaufer@gmail.com>
2020-03-21 20:48:07 -0500

    Message Utils

--- a6/messaging/components/Status.js
+++ a6/messaging/components/Status.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default class Status extends React.Component{
     state = {
-        isConnected: false,
+        isConnected: true,
     };
     async componentWillMount() {
         this.subscription = NetInfo.addEventListener(
