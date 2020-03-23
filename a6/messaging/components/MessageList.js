6b2ebc0894e91aa53fe560551d341bbda749b2a9
Alex Muhich <feldtlaufer@gmail.com>
2020-03-23 14:46:44 -0500

    message list styles

--- a6/messaging/components/MessageList.js
+++ a6/messaging/components/MessageList.js
@@ -37,5 +37,32 @@ const styles = StyleSheet.create({
         flex: 1,
         overflow: 'visible',
     },
+    messageRow: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginBottom: 4,
+        marginRight: 10,
+        marginLeft: 60,
+    },
+    messageBubble: {
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        backgroundColor: 'rgb(16,135,255)',
+        borderRadius: 20,
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 10,
+    },
+    map: {
+        width: 250,
+        height: 250,
+        borderRadius: 10,
+    },
 });
 
