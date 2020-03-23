598bd2f9b633d4a5d821bcd0231dc0264cdac665
Alex Muhich <feldtlaufer@gmail.com>
2020-03-23 15:26:37 -0500

    pre alert, get warnings though

--- a6/messaging/components/MessageList.js
+++ a6/messaging/components/MessageList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {MessageShape} from '../utils/MessageUtils';
+import { MessageShape } from '../utils/MessageUtils';
 import { FlatList, Image, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
