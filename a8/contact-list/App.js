ce5e40730cf29a6d7940b24643f31360ef3dcc59
Alex Muhich <feldtlaufer@gmail.com>
2020-04-24 14:40:08 -0500

    Contacts

--- a8/contact-list/App.js
+++ a8/contact-list/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Contacts from './screens/Contacts';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Contacts />;
 }
 
 const styles = StyleSheet.create({
