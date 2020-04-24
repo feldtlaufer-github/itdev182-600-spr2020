060806e6629e029e1e9d136cb6207c3a35eb9d77
Alex Muhich <feldtlaufer@gmail.com>
2020-04-24 15:32:53 -0500

    favorites test

--- a8/contact-list/routes.js
+++ a8/contact-list/routes.js
@@ -2,13 +2,15 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import Favorites from './screens/Favorites';
 
 const StackNavigator = createStackNavigator({
     Contacts,
     Profile,
+    Favorites,
     },
     {
-      initialRouteName: 'Contacts',
+      initialRouteName: 'Favorites',
 });
 
 export default createAppContainer(StackNavigator);
