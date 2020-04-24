f9b71fc573f49007a167c04bdc37584ddfceb84f
Alex Muhich <feldtlaufer@gmail.com>
2020-04-24 14:47:46 -0500

    Profile

--- a8/contact-list/App.js
+++ a8/contact-list/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
 
 export default function App() {
-  return <Contacts />;
+  return <Profile />;
 }
 
 const styles = StyleSheet.create({
